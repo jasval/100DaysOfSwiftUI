b0dbd5ae8738e2c2e5fc1186e093b0a853bd58d9
J. Rose <saldana.val@gmail.com>
2020-09-08 12:02:27 -0300

    Day 16 activities on the 100 day journey.
    -Previous days are not included as they are mostly revision on basic Swift topics.

---
+++ Splitting (WeSplit)/Splitting/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Splitting
+//
+//  Created by Josh Valdivia on 08/09/2020.
+//  Copyright © 2020 Return Generics. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	// Apple recommends adding private access control
+	@State private var tapCount = 0
+	
+	@State private var name = ""
+	
+    var body: some View {
+		Form {
+			TextField("Enter your name", text: $name)
+			Text("Your name is: \(name)")
+		}
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
